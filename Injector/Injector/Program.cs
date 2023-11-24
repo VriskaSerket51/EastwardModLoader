06fa1937117ce492c7e6984c694f0ca9c49a190b
vriskaserket51 <serketvriska51@gmail.com>
2023-11-24 15:51:14 +0900

    Initial Commit

--- Injector/Injector/Program.cs
+++ Injector/Injector/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EastwardLib.Assets;
 
-Console.WriteLine("Hello, World!");
+if (args.Length < 2)
+{
+    Console.WriteLine("usage: arg0 for _system.g path, arg1 for main.lua path");
+    return;
+}
+
+if (!File.Exists(args[0]))
+{
+    Console.WriteLine("arg0 file not exist");
+    return;
+}
+
+if (!File.Exists(args[1]))
+{
+    Console.WriteLine("arg1 file not exist");
+    return;
+}
+
+var g = GArchive.Read(args[0]);
+g["main.lua"] = new TextAsset(File.ReadAllText(args[1])).Encode();
+File.Move(args[0], $"{args[0]}.bk");
+g.Write(args[0]);
